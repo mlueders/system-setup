6bdaaaf632939465e46e0aa2f308f2f092f78088
Mike Lueders <mlueders@hoovers.com>
2011-11-05 15:40:41 -0500

    added aliases

--- bin/setup-env.sh
+++ bin/setup-env.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 export JAVA_HOME=/usr/lib/jvm/java-6-sun
+export ANT_HOME=/opt/ant
 export GROOVY_HOME=/opt/groovy
 export GRADLE_HOME=/opt/gradle
+export SCALA_HOME=/opt/scala
 
 alias rmbak="rm *~"
+alias jbkill="pkill -9 -f jboss"
+alias clean-ivy="rm -rf ~/.ant"
