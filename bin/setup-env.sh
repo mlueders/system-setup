c66095984e8149374784229b054ee829087e69a6
Mike Lueders <mike@roadkill.org>
2011-12-28 17:45:44 -0600

    more env stuff

--- bin/setup-env.sh
+++ bin/setup-env.sh
@@ -4,9 +4,10 @@ export JAVA_HOME=/usr/lib/jvm/java-6-sun
 export ANT_HOME=/opt/ant
 export GROOVY_HOME=/opt/groovy
 export GRADLE_HOME=/opt/gradle
+export GRAILS_HOME=/opt/grails
 export SCALA_HOME=/opt/scala
 
-export PATH=$PATH:$JAVA_HOME/bin:$ANT_HOME/bin
+export PATH=$PATH:$JAVA_HOME/bin:$ANT_HOME/bin:$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRADLE_HOME/bin:$SCALA_HOME/bin
 
 alias rmbak="rm *~ .*~"
 alias jbkill="pkill -9 -f jboss"
