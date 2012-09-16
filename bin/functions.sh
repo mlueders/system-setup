fb3a9ec841b3488137708ba5f720902a6c5062c9
Mike Lueders <mlueders@hoovers.com>
2012-09-15 19:07:12 -0500

    updated function addkey to return rather than exit

--- bin/functions.sh
+++ bin/functions.sh
@@ -5,13 +5,13 @@ addkey() {
         HOST=${1}.hoovers.com
     else
         echo "No host given"
-        exit 1
+        return
     fi
 
     FOUND=`nslookup $HOST | grep -c "can't find"`
     if [ "$FOUND" == "1" ]; then
         echo "No such host '$HOST'"
-        exit 1
+        return
     fi
 
     COMMAND='mkdir -p .ssh; cat >> .ssh/authorized_keys; chmod 700 .ssh; chmod 600 .ssh/authorized_keys'
@@ -27,3 +27,18 @@ ant-debug() {
     export ANT_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=4142"
     ant $*
 }
+
+jbh() {
+    cd /data/jboss
+}
+
+starttc() {
+    export JAVA_OPTS="-Djboss.server.home.dir=file:///data/jboss/server/balboa -Djboss.server.config.url=file:///data/jboss/server/balboa/conf/"
+    echo "export CLASSPATH=/data/jboss/server/balboa/conf" > /opt/tomcat6/bin/setenv.sh
+    /opt/tomcat6/bin/catalina.sh start
+}
+
+stoptc() {
+    pkill -9 -f org.apache.catalina.startup.Bootstrap
+}
+
