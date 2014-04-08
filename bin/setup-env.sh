2a4d3897a80651baabb6b16a4d46234705f370c2
Mike Lueders <mike@roadkill.org>
2014-04-08 16:10:20 -0500

    removed ant/gant from path, added gradle aliases, incorporated git-completion.bash

--- bin/setup-env.sh
+++ bin/setup-env.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-export JAVA_HOME=/opt/JVMVersions/1.6/Home
-export ANT_HOME=/opt/ant
-export GANT_HOME=/opt/gant
+export JAVA_HOME=/opt/JVMVersions/Current/Home
 export GROOVY_HOME=/opt/groovy
 export GRADLE_HOME=/opt/gradle
 export GRAILS_HOME=/opt/grails
 export SCALA_HOME=/opt/scala
 
-export PATH=$PATH:$JAVA_HOME/bin:$ANT_HOME/bin:$GANT_HOME/bin:$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRADLE_HOME/bin:$SCALA_HOME/bin
+export PATH=/usr/local/bin:$PATH:~/bin:$JAVA_HOME/bin:$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRADLE_HOME/bin:$SCALA_HOME/bin
 
 alias rmbak="rm *~ .*~"
 alias jbh="cd /data/jboss"
@@ -16,5 +14,12 @@ alias jbkill="pkill -9 -f jboss"
 alias clean-ivy="rm -rf ~/.ant"
 alias stop-ssh="sudo /etc/init.d/ssh stop"
 alias start-ssh="sudo /etc/init.d/ssh start"
+alias gr="gradle --daemon"
+alias gw="./gradlew --daemon"
 
 source ~/bin/functions.sh
+source ~/bin/git-completion.bash
+
+# ruby initialization
+export RBENV_ROOT=/usr/local/var/rbenv
+eval "$(rbenv init -)"
