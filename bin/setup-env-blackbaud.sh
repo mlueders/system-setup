30e4b076da85fb2f3859da57c8646640aae9efe1
Mike Lueders <Mike.Lueders@blackbaud.com>
2014-10-06 17:46:18 -0500

    add s3cmd to path; load blackbaud configuration as separate file

---
+++ bin/setup-env-blackbaud.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LUMINATE_HOME=/Users/mlueders/IdeaProjects/Blackbaud/luminate-online
+
+source ~/bin/functions-blackbaud.sh
--- bin/setup-env.sh
+++ bin/setup-env.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-export DOCKER_HOST=tcp://localhost:4243
+export DOCKER_HOST=tcp://192.168.55.55:2375
 export JAVA_HOME=/opt/JVMVersions/CurrentJDK/Home
 export GROOVY_HOME=/opt/groovy
 export GRADLE_HOME=/opt/gradle
 export GRAILS_HOME=/opt/grails
 export SCALA_HOME=/opt/scala
+export S3CMD_HOME=/opt/s3cmd
 export M2_HOME=/opt/maven
 export MAVEN_OPTS="-Xmx2048m -XX:PermSize=128m"
 
 export PS1="\w$ "
 
-export PATH=/usr/local/bin:$PATH:~/bin:$JAVA_HOME/bin:$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRADLE_HOME/bin:$SCALA_HOME/bin:$M2_HOME/bin
+export PATH=/usr/local/bin:$PATH:~/bin:$JAVA_HOME/bin:$GROOVY_HOME/bin:$GRAILS_HOME/bin:$GRADLE_HOME/bin:$SCALA_HOME/bin:$M2_HOME/bin:$S3CMD_HOME
 
 alias rmbak="rm *~ .*~"
 alias jbkill="pkill -9 -f jboss"
@@ -21,7 +22,6 @@ alias gr="gradle --daemon"
 alias gw="./gradlew --daemon"
 
 source ~/bin/functions.sh
-source ~/bin/functions-blackbaud.sh
 source ~/bin/git-completion.bash
 
 # ruby initialization
