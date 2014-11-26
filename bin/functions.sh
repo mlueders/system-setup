8788553c3b8530c0dd862af97d340b3cac6deeaf
Mike Lueders <mike@roadkill.org>
2014-11-25 18:10:05 -0600

    added swjava option to pass in index on command line

--- bin/functions.sh
+++ bin/functions.sh
@@ -78,10 +78,17 @@ awscheckip() {
 }
 
 swjava() {
+    local INDEX=$1
     local VERSIONS=`/usr/libexec/java_home -V 2>&1 | grep -E "\d.\d.\d[,_]" | cut -d , -f 1 | colrm 1 4 | grep -v Home | uniq`
     local JVM
 
-    select_from_options JVM "JVM" ${VERSIONS[@]}
+    if [ -z "${INDEX}" ]; then
+        select_from_options JVM "JVM" ${VERSIONS[@]}
+    else
+        local VERSIONS_ARRAY=(${VERSIONS[@]})
+        INDEX=$((INDEX - 1))
+        JVM=${VERSIONS_ARRAY[$INDEX]}
+    fi
 
     export JAVA_HOME=$(/usr/libexec/java_home -v $JVM)
     export PATH=$JAVA_HOME/bin:$PATH
